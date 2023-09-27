b31a97a693904b25846fe3af71a28f20ae883b55
jahid45780 <jahidhossain88899@gmail.com>
2023-09-27 15:26:12 +0600

    add the phone reponsive divise

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -12,9 +12,15 @@ const Banner = () => {
      
     <h2 className=" font-bold text-4xl" > I Grow By Helping People In Need </h2>
  <br />
- <p>  <input className=" bg-slate-300 p-1 w-80 rounded-md "  type="text" name="name" placeholder="Search here.." id="" />
-  <button className=" bg-[#FF444A] p-1 rounded-md px-4 " >Search</button> </p> 
+   <div className=" grid grid-cols-1 lg:grid-cols-2 gap-3" >
 
+   <div> <input className=" bg-slate-300 p-1  w-72 rounded-md "  type="text" name="name" placeholder="Search here.." id="" /> </div>
+  
+  <div> <button className=" bg-[#FF444A] p-1 rounded-md px-7 " >Search</button>  </div> 
+
+
+
+   </div>
 
     </div>
   </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,49 +2,53 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
-        <nav className=" flex justify-between items-center  mt-8" >
+       <div className=" m-3" >
+
+<nav className=" flex justify-between items-center  mt-8" >
             
-    <div>
-        <img className=" w-40" src={'https://i.ibb.co/r6JgcZr/Logo.png'} alt="" />
-    </div>
+            <div>
+                <img className=" w-40" src={'https://i.ibb.co/r6JgcZr/Logo.png'} alt="" />
+            </div>
+        
+            <div className=" flex gap-3" >
+        
+        
+            <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? " text-red-600 underline" : ""
+          }
+        >
+          Home
+        </NavLink>
+        
+        
+        <NavLink
+          to="/donation"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? " text-red-600 underline" : ""
+          }
+        >
+          Donation
+        </NavLink>        
+        
+        
+        <NavLink
+          to="/statistics"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? " text-red-600 underline" : ""
+          }
+        >
+        Statistics
+        </NavLink>  
+        
+        
+        
+            </div>
+        
+                </nav>
 
-    <div className=" flex gap-3" >
-
-
-    <NavLink
-  to="/"
-  className={({ isActive, isPending }) =>
-    isPending ? "pending" : isActive ? " text-red-600 underline" : ""
-  }
->
-  Home
-</NavLink>
-
-
-<NavLink
-  to="/donation"
-  className={({ isActive, isPending }) =>
-    isPending ? "pending" : isActive ? " text-red-600 underline" : ""
-  }
->
-  Donation
-</NavLink>        
-
-
-<NavLink
-  to="/statistics"
-  className={({ isActive, isPending }) =>
-    isPending ? "pending" : isActive ? " text-red-600 underline" : ""
-  }
->
-Statistics
-</NavLink>  
-
-
-
-    </div>
-
-        </nav>
+       </div>
     );
 };
 
